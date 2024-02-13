ee0cc7451d4231f4a69f26a6190473418c915f31
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2024-02-13 06:07:00 +0100

    Project_session7_meetup

---
+++ Section_7_CourseProject_ComponentsDeepDive/Project7/src/App.svelte
@@ -0,0 +1,121 @@
+
+<script>
+  import Header from "./UI/Header.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+  import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
+     let title = '';
+     let subtitle= '';
+     let imageUrl= '';
+     let description= '';
+     let adress= '';
+     let email= '';
+    let meetups = 
+  [
+    {
+      id: 'm1',
+      title: 'persona4r meetup' ,
+      subtitle: 'Learn to play in 2 hours' ,
+      description: 'In this meetup, we will have some experts like chie',
+      imageUrl: 'https://atlus.com/wp-content/uploads/2022/03/persona4.jpg',
+      contactEmail: 'code@test.com',
+      adress: 'nerd Street 420, jeruzalem'
+    },
+    {
+      id: 'm2',
+      title: 'Persona5r meetup' ,
+      subtitle: 'Learn to play in 2 hours' ,
+      description: 'In this meetup, we will have some experts like Joker',
+      imageUrl: 'https://www.spriters-resource.com/resources/sheets/126/129650.png?updated=1586993323',
+      contactEmail: 'code@test.com',
+      adress: 'Epic straat 6969, 9000 Belgium'
+    }
+  ];
+
+  function addMeetup()
+  {
+    const newMeetup =
+    {
+      id: Math.random().toString(),
+      
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      contactEmail: email,
+      adress:  adress,
+    }
+    meetups = [newMeetup,...meetups ];
+  }
+
+  function help (event)
+  {
+    title = event.target.value
+    console.log('HASWEJIFHDSEYUIFHSYHEFHISEFHUJISEF')
+    console.log(event)
+
+  }
+</script>
+
+<style>
+
+  main
+  {
+    margin-top: 5rem;
+  }
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
+</style>
+
+
+  <Header />
+
+  <main>
+    <form on:submit|preventDefault="{addMeetup}"> 
+      <TextInput 
+        id = 'title'
+        type = 'text' 
+        value = {title} 
+        on:input= {help} />
+
+      <TextInput 
+        id = 'subtitle' 
+        type = 'text'
+        value = {subtitle} 
+        on:input= {event => (subtitle = event.target.value)} />
+
+      <TextInput 
+        id = 'adress' 
+        type = 'text'
+        value = {adress} 
+        on:input= {event => (adress = event.target.value)} />
+
+      <TextInput 
+        id = 'imageUrl' 
+        type = 'text'
+        value = {imageUrl} 
+        on:input= {event => (imageUrl = event.target.value)} />
+
+      <TextInput 
+        id = 'email' 
+        value = {email} 
+        type = 'email'
+        on:input= {event => (email = event.target.value)} />
+
+      <TextInput 
+        id = 'description' 
+        controlType = 'textarea'
+        value = {description} 
+        on:input= {event => (description = event.target.value)} />
+
+      <Button type='submit' caption= "Save" />
+    </form>
+    <MeetupGrid {meetups} />
+  </main>
+
+
+
+
---
+++ Section_7_CourseProject_ComponentsDeepDive/Project7/src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,36 @@
+<script>
+
+    import MeetupItems from "./MeetupItems.svelte";
+
+    export let meetups
+    
+</script>
+
+<style>
+    section {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+}
+
+@media (min-width: 768px) {
+  section {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+</style>
+
+<section id="meetups">
+    {#each meetups as meetup}
+      <MeetupItems
+      title = {meetup.title}
+      subtitle = {meetup.subtitle}
+      imageUrl = {meetup.imageUrl}
+      description = {meetup.description}
+      adress = {meetup.adress}
+      mail = {meetup.contactEmail}
+      />
+    {/each}
+  </section>  

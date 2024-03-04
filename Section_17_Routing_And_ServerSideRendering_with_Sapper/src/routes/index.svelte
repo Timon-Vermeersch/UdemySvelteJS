7b58406f61623cb48f91f700414797916a0a492c
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2024-03-04 14:11:02 +0100

    Sapper

--- Section_17_Routing_And_ServerSideRendering_with_Sapper/src/routes/index.svelte
+++ Section_17_Routing_And_ServerSideRendering_with_Sapper/src/routes/index.svelte
@@ -1,46 +1,133 @@
+<script>
+  import { createEventDispatcher,onMount } from "svelte";
+  import { scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
+  import MeetupItem from "../components/MeetupItem.svelte";
+  import MeetupFilter from "../components/Meetups/MeetupFilter.svelte"
+  import Button from "../components/UI/Button.svelte";
+  import EditMeetup from "../components/Meetups/EditMeetup.svelte";
+  import LoadingSpinner from "../components/UI/LoadingSpinner.svelte"
+  
+  
+  let meetups = [];
+
+
+  let editMode;
+  let editedId;
+  let isLoading = true;
+  
+  onMount(() => {
+    fetch("https://svelte-course-981bc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json")
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Fetching meetups failed, please try again later!");
+      }
+      return res.json();
+    })
+    .then(data => {
+      const loadedMeetups = [];
+      for (const key in data) {
+        loadedMeetups.push({
+          ...data[key],
+          id: key
+        });
+      }
+      setTimeout(() => {
+        isLoading = false;
+        meetups.setMeetups(loadedMeetups.reverse());
+      }, 1000);
+    })
+    .catch(err => {
+      error = err;
+      isLoading = false;
+      console.log(err);
+    });
+  })
+  
+  
+  const dispatch = createEventDispatcher();
+
+  let favsOnly = false;
+
+  $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
+
+  function setFilter(event) {
+    favsOnly = event.detail === 1;
+  }
+
+  function savedMeetup(event) {
+    editMode = null;
+    editedId = null;
+  }
+
+  function cancelEdit() {
+    editMode = null;
+    editedId = null;
+  }
+
+
+
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  #meetups {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  #meetup-controls {
+    margin: 1rem;
+    display: flex;
+    justify-content: space-between;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  #no-meetups {
+    margin: 1rem;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 768px) {
+    #meetups {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>All Meetups</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+{#if editMode === 'edit'}
+      <EditMeetup id={editedId} on:save={savedMeetup} on:cancel={cancelEdit} />
+    {/if}
+    {#if isLoading}
+      <LoadingSpinner />
+    {:else}
+      <section id="meetup-controls">
+        <MeetupFilter on:select={setFilter} />
+        <Button on:click={() => dispatch('add')}>New Meetup</Button>
+      </section>
+      {#if filteredMeetups.length === 0}
+        <p id="no-meetups">No meetups found, you can start adding some.</p>
+      {/if}
+      <section id="meetups">
+        {#each filteredMeetups as meetup (meetup.id)}
+          <div transition:scale animate:flip={{ duration: 300 }}>
+            <MeetupItem
+              id={meetup.id}
+              title={meetup.title}
+              subtitle={meetup.subtitle}
+              description={meetup.description}
+              imageUrl={meetup.imageUrl}
+              email={meetup.contactEmail}
+              address={meetup.address}
+              isFav={meetup.isFavorite}
+              on:showdetails
+              on:edit />
+          </div>
+        {/each}
+      </section>
+    
+    {/if}
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>Uw moeder FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
